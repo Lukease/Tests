bba7eeebf23debb09e26a839e75bb94906c5b1f5
Lukease <lukpawlowski1996@gmail.com>
2022-02-12 10:55:39 +0100

    Revert"utworzenie nowej klasy o nazwie FplLeague"

---
+++ src/test/java/FplLeague.java
@@ -0,0 +1,65 @@
+public class FplLeague {
+    private String leagueName;
+    private String teamName;
+    private int rank;
+    private int gameWeekPoints;
+    private int totalPoints;
+    private String manager;
+
+    public FplLeague(String leagueName, String teamName, int rank, int gameWeekPoints, int totalPoints, String manager) {
+        this.leagueName = leagueName;
+        this.teamName = teamName;
+        this.rank = rank;
+        this.gameWeekPoints = gameWeekPoints;
+        this.totalPoints = totalPoints;
+        this.manager = manager;
+    }
+
+    public String getLeagueName() {
+        return leagueName;
+    }
+
+    public void setLeagueName(String leagueName) {
+        this.leagueName = leagueName;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getGameWeekPoints() {
+        return gameWeekPoints;
+    }
+
+    public void setGameWeekPoints(int gameWeekPoints) {
+        this.gameWeekPoints = gameWeekPoints;
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
+
+    public String getManager() {
+        return manager;
+    }
+
+    public void setManager(String manager) {
+        this.manager = manager;
+    }
+}
