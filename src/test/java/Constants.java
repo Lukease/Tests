2d72990a2ca778a8ab6439e149261004f37a90ba
Lukease <lukpawlowski1996@gmail.com>
2022-02-17 11:02:24 +0100

    Add new clubNames in Constants class.

--- src/test/java/Constants.java
+++ src/test/java/Constants.java
@@ -6,10 +6,25 @@ public class Constants {
 
     public Constants() {
         this.clubNames.put("LIV", "Liverpool");
-        this.clubNames.put("MCI", "Manchester City");
+        this.clubNames.put("MCI", "Man City");
         this.clubNames.put("AVL", "Aston Villa");
         this.clubNames.put("WHU", "West Ham");
+        this.clubNames.put("WOL", "Wolves");
         this.clubNames.put("WAT", "Watford");
+        this.clubNames.put("TOT", "Spurs");
+        this.clubNames.put("SOU", "Southampton");
+        this.clubNames.put("NOR", "Norwich");
+        this.clubNames.put("NEW", "Newcastle");
+        this.clubNames.put("MUN", "Man Udt");
+        this.clubNames.put("LEI", "Leicester");
+        this.clubNames.put("LEE", "Leeds");
+        this.clubNames.put("EVE", "Everton");
+        this.clubNames.put("CRY", "Crystal Palace");
+        this.clubNames.put("CHE", "Chelsea");
+        this.clubNames.put("BUR", "Burnley");
+        this.clubNames.put("BHA", "Brighton");
+        this.clubNames.put("BRE", "Brentford");
+        this.clubNames.put("ARS", "Arsenal");
         this.playerPositions.put("GKP", "Goalkeeper");
         this.playerPositions.put("DEF", "Defender");
         this.playerPositions.put("MID", "Miedfielder");
